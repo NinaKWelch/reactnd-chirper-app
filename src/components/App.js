5b214afcac281a17ab6c9f1cea2f625c472b034d
NinaWelch <nwelch1@live.co.uk>
2020-07-07 13:50:51 +0100

    completed project

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import LoadingBar from 'react-redux-loading'
+import Dashboard from './Dashboard'
+import TweetPage from './TweetPage'
+import NewTweet from './NewTweet'
+import Nav from './Nav'
 
-function App() {
-  return (
-    <div>
-      Starter Code
-    </div>
-  );
+/* Using the connect() function upgrades a component to a container. 
+Containers can read state from the store and dispatch actions. */
+
+class App  extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          
+          <div className='container'>
+            <Nav />
+            {this.props.loading === true
+              ? null
+              : <div>
+                  <Route path='/' exact component={Dashboard} />
+                  <Route path='/tweet/:id' component={TweetPage} />
+                  <Route path='/new' component={NewTweet} />
+                </div>
+            }
+          </div>
+        </Fragment>
+      </Router>
+    )
+  }
 }
 
-export default App;
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
